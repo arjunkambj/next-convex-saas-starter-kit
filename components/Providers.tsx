90289680fe4fa21847d92082e79b3ff817582fed
Honey <arjunkamboj87699@gmail.com>
2025-08-24 17:01:51 +0000

    feat: Basic Convex Setup

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { ConvexClientProvider } from "@/components/shared/ConvexClientProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <HeroUIProvider>
+      <ConvexClientProvider>{children}</ConvexClientProvider>
+    </HeroUIProvider>
+  );
 }
